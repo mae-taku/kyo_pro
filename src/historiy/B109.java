86253c2089ab8d461051a5506e9e8d3e3d6d0451
前田拓海 <maetaku@localhost>
2022-11-08 14:11:52 +0900

    B109 修正

--- src/historiy/B109.java
+++ src/historiy/B109.java
@@ -1,5 +1,6 @@
 package historiy;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,58 +23,85 @@ public class B109 {
 		//		String line = "9 4 5 2 3 ";
 		String[] inpuStrings = line.split(" ", 0);
 		//		String[] dummy = { "1 0", "1 2", "1 3", "1 4", "2 2", "2 3", "2 4", "3 3", "3 4" };
-		List<String> bookedSheet = new ArrayList<>();
+
+		//		dummy[0]="3.0";
+		List<Point> bookedSheet = new ArrayList<>();
 		for (int i = 0; i < Integer.parseInt(inpuStrings[0]); i++) {
 			String booked = sc.nextLine();
-			//			String booked = dummy[i];
-			bookedSheet.add(booked);
+			//			String[] temp = dummy[i].split(" ", 0);
+			String[] temp = booked.split(" ", 0);
+			Point pbooked = new Point(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
+			bookedSheet.add(pbooked);
 		}
+		//		見やすい席をリストアップ
 
-		List<String> sheet = new ArrayList<>();
+		List<sheet> sheet = new ArrayList<>();
 		for (int i = 0; i < Integer.parseInt(inpuStrings[1]); i++) {
 			for (int j = 0; j < Integer.parseInt(inpuStrings[2]); j++) {
-				sheet.add(i + " " + j);
+				Point p = new Point(i, j);
+				sheet s = new sheet(p);
+				int prio = 0;
+				int n = 0;
+				int m = 0;
+				n = Math.abs(
+						(Integer.parseInt(inpuStrings[3]) - i));
+				m = Math.abs((Integer.parseInt(inpuStrings[4]) - j));
+				prio = n + m;
+				s.setPrio(prio);
+				sheet.add(s);
 			}
 		}
-		for (String s : bookedSheet) {
-			sheet.removeIf(n -> (n.matches(s)));
-		}
-		List<String[]> sheetList = new ArrayList<>();
-		for (String s : sheet) {
-			String[] temp = new String[2];
-			String num[] = s.split(" ", 0);
-			int prio = 0;
-			int n = 0;
-			int m = 0;
-			n = Math.abs(
-					(Integer.parseInt(inpuStrings[3]) - Integer.parseInt(num[0])));
-			m = Math.abs((Integer.parseInt(inpuStrings[4]) - Integer.parseInt(num[1])));
-			prio = n + m;
-			temp[0] = Integer.valueOf(prio).toString();
-			temp[1] = s;
-			sheetList.add(temp);
 
-		}
-		int count = 0;
-		boolean stop = false;
-		List<String> result = new ArrayList<>();
-		for (int j = 0; j < sheetList.size(); j++) {
-			for (int i = 1; i < sheetList.size(); i++) {
-				String[] str = new String[2];
-				str = sheetList.get(i);
-				if (count == Integer.parseInt(str[0])) {
-					result.add(str[1]);
-					stop = true;
+		for (int i = 0; i < sheet.size(); i++) {
+			for (Point p : bookedSheet) {
+				if (sheet.get(i).getsheet().equals(p)) {
+					sheet.remove(i);
+					i--;
+					break;
 				}
 			}
-			if (stop) {
-				break;
-			}
-			count++;
 		}
-		result.forEach(s -> System.out.println(s));
-		//		int target = (P - p_n) + (Q - q_n);
-		//        num1 = Math.abs(num1);
+		//		全席の優先順位を0から順に取り出す。
+		int index = 0;
+		List<String> ans = new ArrayList<>();
+		while (ans.isEmpty()) {
+			for (sheet s : sheet) {
+				if (s.getprio() == index) {
+					ans.add(s.toString());
+				}
+			}
+			index++;
+		}
 
+		ans.forEach(s -> System.out.println(s));
+
+	}
+
+	static class sheet {
+		Point p;
+		int i;
+
+		public sheet(Point point) {
+			// TODO 自動生成されたコンストラクター・スタブ
+			this.p = point;
+		}
+
+		public String toString() {
+			int x = (int) p.getX();
+			int y = (int) p.getY();
+			return x + " " + y;
+		}
+
+		public Point getsheet() {
+			return this.p;
+		}
+
+		public void setPrio(int i) {
+			this.i = i;
+		}
+
+		public int getprio() {
+			return this.i;
+		}
 	}
 }
